c691612aeffcbd7f4520fc8be00d03abc787a46f
Mariano Romano <mromano@cribl.io>
2024-09-06 17:49:25 -0400

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Mariano Romano on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
